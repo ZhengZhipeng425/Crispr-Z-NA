383d346d063ce5869dbe26acfbaf769c97fe07e1
ZhengZhipeng425 <97509755+ZhengZhipeng425@users.noreply.github.com>
2025-03-12 04:12:03 +0800

    GO Enrich demo

---
+++ GO_enrich/GO_enrich.R
@@ -0,0 +1,24 @@
+library(ggplot2)#柱状图和点状图
+library(enrichplot)#GO,KEGG,GSEA
+library(clusterProfiler)#GO,KEGG,GSEA
+library(GOplot)#弦图，弦表图，系统聚类图
+library(DOSE)
+library(ggnewscale)
+library(topGO)#绘制通路网络图
+library(circlize)#绘制富集分析圈图
+library(ComplexHeatmap)#绘制图例
+library(org.Hs.eg.db)
+
+#指定富集分析的物种库
+GO_database <- 'org.Hs.eg.db' #GO分析指定物种，物种缩写索引表详见http://bioconductor.org/packages/release/BiocViews.html#___OrgDb
+GO<-enrichGO( LP$id,#GO富集分析
+              OrgDb = GO_database,
+              keyType = "ENTREZID",#设定读取的gene ID类型
+              ont = "ALL",#(ont为ALL因此包括 Biological Process,Cellular Component,Mollecular Function三部分）
+              pvalueCutoff = 0.05,#设定p值阈值
+              qvalueCutoff = 0.05,#设定q值阈值
+              readable = T )
+
+barplot(GO, split="ONTOLOGY") +
+  facet_grid(ONTOLOGY~., scale="free_x") + #柱状
+  xlim(0, max(GO@result$Count) * 1.5)
